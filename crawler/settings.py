9964f35a2f0c4513a2d03b999bb2b230ae4ba70d
Kai Yan <u3514123@connect.hku.hk>
2018-11-06 15:30:44 +0800

    private settings

--- crawler/settings.py
+++ crawler/settings.py
@@ -8,7 +8,7 @@ class DBConfig(object):
 		self.host = 'localhost', 
 		self.port = 3306,
 		self.user = 'root', 
-		self.passwd = 'admin@0225', 
+		self.passwd = '', 
 		self.db = 'seekingalpha', 
 		self.charset = 'utf8'
 		
@@ -19,7 +19,7 @@ db_config = DBConfig()
 # http://www.moguproxy.com/
 class MoguConfig(object):
 	def __init__(self):
-		self.appKey = "U1BpNGxpMVZjbDcxNjJPeDpIaDhFYkdoem9HR2tvdW5n"
+		self.appKey = ""
 		self.ip_port = 'transfer.mogumiao.com:9001'
 
 mogu_config = MoguConfig()
@@ -38,8 +38,8 @@ class ProxyConfig(object):
 abuyun_config = ProxyConfig(
 	host = "http-dyn.abuyun.com",
 	port = 9020,
-	username = "HY939725721Z1ZYD",
-	password = "83A19E7CB307DE92")
+	username = "",
+	password = "")
 
 
 # Luminati proxy configuration 
@@ -47,10 +47,8 @@ abuyun_config = ProxyConfig(
 luminati_config = ProxyConfig(
 	host = "zproxy.lum-superproxy.io",
 	port = 22225,
-	# username = "lum-customer-hl_68fc3a66-zone-zone1",
-	# password = "kty7qy02zrrn")
-	username = "lum-customer-hl_68fc3a66-zone-zone2",
-	password = "3ljrgdqhlp73")
+	username = "",
+	password = "")
 
 # SeekingAlpha address
 ROOT_1 = "https://seekingalpha.com/earnings/earnings-call-transcripts/"
